3d2fdedf381fd8da33ebc1369313293cab40c6d4
root <rajeev.sharma@jadeglobal.com>
2018-11-27 12:57:02 +0530

    added topic modelling

---
+++ machine_learning/topic_modelling.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import gensim
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+import nltk
+import numpy as np
+from gensim import models
+
+nltk.download('wordnet')
+
+data = pd.read_csv('abcnews-date-text.csv', error_bad_lines=False)
+data_text = data[['headline_text']]
+data_text['index'] = data_text.index
+documents = data_text
+
+print(len(documents))
+print(documents[:5])
+
+np.random.seed(2018)
+
+print(WordNetLemmatizer().lemmatize('went', pos='v'))
+
+### Performing stem operation
+stemmer = SnowballStemmer('english')
+original_words = ['caresses', 'flies', 'dies', 'mules', 'denied','died', 'agreed', 'owned',
+           'humbled', 'sized','meeting', 'stating', 'siezing', 'itemization','sensational',
+           'traditional', 'reference', 'colonizer','plotted']
+singles = [stemmer.stem(plural) for plural in original_words]
+chk = pd.DataFrame(data = {'original word': original_words, 'stemmed': singles})
+print(chk)
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+doc_sample = documents[documents['index'] == 4310].values[0][0]
+
+print('original document: ')
+words = []
+for word in doc_sample.split(' '):
+    words.append(word)
+print(words)
+print('\n\n tokenized and lemmatized document: ')
+print(preprocess(doc_sample))
+
+processed_docs = documents['headline_text'].map(preprocess)
+print(processed_docs[:10])
+
+#### Bagging words on the dataset.
+dictionary = gensim.corpora.Dictionary(processed_docs)
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+print(bow_corpus[4310])
+
+bow_doc_4310 = bow_corpus[4310]
+
+for i in range(len(bow_doc_4310)):
+    print("Word {} (\"{}\") appears {} time.".format(bow_doc_4310[i][0],
+                                                     dictionary[bow_doc_4310[i][0]],
+                                                     bow_doc_4310[i][1]))
+
+### Performing TF-IDF
+tfidf = models.TfidfModel(bow_corpus)
+corpus_tfidf = tfidf[bow_corpus]
+from pprint import pprint
+
+for doc in corpus_tfidf:
+    pprint(doc)
+    break
+
+lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
+
+for idx, topic in lda_model.print_topics(-1):
+    print('Topic: {} \nWords: {}'.format(idx, topic))
+
+lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=10, id2word=dictionary, passes=2, workers=4)
+for idx, topic in lda_model_tfidf.print_topics(-1):
+    print('Topic: {} Word: {}'.format(idx, topic))
+
+print(processed_docs[4310])
+
+for index, score in sorted(lda_model[bow_corpus[4310]], key=lambda tup: -1*tup[1]):
+    print("\nScore: {}\t \nTopic: {}".format(score, lda_model.print_topic(index, 10)))
+
+for index, score in sorted(lda_model_tfidf[bow_corpus[4310]], key=lambda tup: -1*tup[1]):
+    print("\nScore: {}\t \nTopic: {}".format(score, lda_model_tfidf.print_topic(index, 10)))
+
+unseen_document = 'How a Pentagon deal became an identity crisis for Google'
+bow_vector = dictionary.doc2bow(preprocess(unseen_document))
+
+for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
+    print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))
+
